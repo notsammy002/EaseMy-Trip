051988f6abebf7749115ab2683f3e887977ba752
VishalChauhan562 <chauhanvishal562@gmail.com>
2022-04-01 17:03:06 +0530

    checkout isssues solved2

--- easemy-trip/src/componentsB/authentication/Login.jsx
+++ easemy-trip/src/componentsB/authentication/Login.jsx
@@ -26,7 +26,7 @@ export const Login = ({ isToken, setIsToken }) => {
 
         console.log(loginData)
 
-        const response = await fetch('/auth/login', {
+        const response = await fetch('http://localhost:8000/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(loginData)
